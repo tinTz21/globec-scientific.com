f001057c70c546e97fcc788461b059db490b758c
Augustino Emanuel <augustino.emanuel73@gmail.com>
2023-06-02 02:49:34 +0300

    dashboard home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,20 +1,106 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+<div class="container-fluid">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-11">
             <div class="card">
                 <div class="card-header">{{ __('Dashboard') }}</div>
 
                 <div class="card-body">
                     @if (session('status'))
                         <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
+                            {{ session('status') }} 112
                         </div>
                     @endif
 
-                    {{ __('You are logged in!') }}
+                    <div class="row">
+                        <div class="col-md-4" style="margin-top: 1em;">
+                            <div class="card">
+                                <div class="card-header">
+                                    Products
+                                </div>
+                                <div class="card-body">
+                                    
+                                </div>
+                                <div class="card-footer">
+                                    
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-md-4" style="margin-top: 1em;">
+                            <div class="card">
+                                <div class="card-header">
+                                    Training
+                                </div>
+                                <div class="card-body">
+                                    
+                                </div>
+                                <div class="card-footer">
+                                    
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-md-4" style="margin-top: 1em;">
+                            <div class="card">
+                                <div class="card-header">
+                                    
+                                </div>
+                                <div class="card-body">
+                                    
+                                </div>
+                                <div class="card-footer">
+                                    
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-md-4" style="margin-top: 1em;">
+                            <div class="card">
+                                <div class="card-header">
+                                    
+                                </div>
+                                <div class="card-body">
+                                    
+                                </div>
+                                <div class="card-footer">
+                                    
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-md-4" style="margin-top: 1em;">
+                            <div class="card">
+                                <div class="card-header">
+                                    
+                                </div>
+                                <div class="card-body">
+                                    
+                                </div>
+                                <div class="card-footer">
+                                    
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-md-4" style="margin-top: 1em;">
+                            <div class="card">
+                                <div class="card-header">
+                                    Blogs
+                                </div>
+                                <div class="card-body">
+                                    
+                                </div>
+                                <div class="card-footer">
+                                    
+                                </div>
+                            </div>
+                        </div>
+
+
+                    </div>
                 </div>
             </div>
         </div>
---
+++ resources/views/livewire/home/footer.blade.php
@@ -0,0 +1,93 @@
+<div>
+    {{-- Knowing others is intelligence; knowing yourself is true wisdom. --}}
+    <div class="container-fluid bg-light" style="padding: 1.2em;">
+        <div class="row">
+            <div class="col-md-12" style="padding: 1em">
+                <div class="row">
+                    <div class="col-md-3 ">
+                        <h3 class="d-flex justify-content-center">
+                            About Us
+                        </h3>
+                        <p class="d-flex justify-content-center" style="padding-left:2em">
+                            This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer. This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer. 
+                        </p>
+                    </div>
+                    <div class="col-md-3 ">
+                        <h3 class="d-flex justify-content-center">
+                            Quick Links
+                        </h3>
+                        <p> 
+                            <div>
+                                <a href="#" style="text-decoration: none; padding-left: 1em;" class="d-flex justify-content-left">
+                                    <i class="bi-dot"></i> PRODUCTS
+                                </a>
+                            </div>
+
+                            <div>
+                                <a href="#" style="text-decoration: none; padding-left: 1em;" class="d-flex justify-content-left">
+                                    <i class="bi-dot"></i> KNOWLEDGE & SUPPORT
+                                </a>
+                            </div>
+
+                            <div>
+                                <a href="#" style="text-decoration: none; padding-left: 1em;" class="d-flex justify-content-left">
+                                    <i class="bi-dot"></i> CONTACT US
+                                </a>
+                            </div>
+                            <div>
+                                <a href="#" style="text-decoration: none; padding-left: 1em;" class="d-flex justify-content-left">
+                                    <i class="bi-dot"></i> BLOG
+                                </a>    
+                            </div>
+                            
+                        </p>
+                    </div>
+                    <div class="col-md-3">
+                        <h3 class="d-flex justify-content-center">
+                            Update & News
+                        </h3>
+                        
+                        <a href="#" class="d-flex justify-content-left" style="text-decoration: none; padding-left: 1em;">
+                            <i class="bi-dot"></i> Multianalyte liquid biopsy
+                        </a>
+                        <a href="#" class="d-flex justify-content-left" style="text-decoration: none; padding-left: 1em;">
+                            <i class="bi-dot"></i> World Cancer Day highlights value of research
+                        </a>
+                        <a href="#" class="d-flex justify-content-left" style="text-decoration: none; padding-left: 1em;">
+                            <i class="bi-dot"></i> Sensitive mutation analysis of cfDNA by dPCR Q&As
+                        </a>
+                        <a href="#" class="d-flex justify-content-left" style="text-decoration: none; padding-left: 1em;">
+                            <i class="bi-dot"></i> Cell-free DNA from liquid biopsies
+                        </a>
+                        <a href="#" class="d-flex justify-content-left" style="text-decoration: none; padding-left: 1em;">
+                            <i class="bi-dot"></i> Multianalyte liquid biopsy
+                        </a>
+                       
+                        
+                    </div>
+
+                    <div class="col-md-3 d-flex justify-content-center" style="margin-top: 1em">
+                        <h3>
+                            Latest Products
+                        </h3>
+                    </div>
+                </div>
+            </div>
+            <hr>
+            <div class="col-md-6">
+                {{ '@ '. now()->year .' | All Rights Reserved by Globec Scientific Ltd | '}} <a href="#" style="text-decoration: none;">Privacy & Policy</a> 
+            </div>
+            <div class="col-md-6 d-flex justify-content-end">
+                <i class="bi-youtube text-danger"></i> <a href="#" style="text-decoration: none">Youtube</a>
+                &nbsp; &nbsp;
+                <i class="bi-twitter text-primary"></i> <a href="#" style="text-decoration: none">Twitter</a>
+                &nbsp;&nbsp;
+                <i class="bi-instagram text-warning"></i> <a href="#" style="text-decoration: none">Instagram</a>
+                &nbsp;&nbsp;
+                <i class="bi-facebook"></i> <a href="#" style="text-decoration: none">FaceBook</a>
+                &nbsp; | &nbsp;&nbsp;
+                <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline" style="text-decoration: none;">Log-in</a>
+            </div>
+        </div>
+    </div>
+</div>
