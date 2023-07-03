c5db91cd680787c367a9098d2439a704fd70f05d
Augustino Emanuel <augustino.emanuel73@gmail.com>
2023-07-03 23:00:07 +0300

    sliding image

--- resources/views/livewire/home/show-home.blade.php
+++ resources/views/livewire/home/show-home.blade.php
@@ -19,29 +19,37 @@
             <div class="col-md-6" style="padding: 2em">
 
                 <div id="carouselExampleCaptions" class="carousel slide">
-                      <div class="carousel-indicators">
-                        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-                        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
-                        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
-                      </div>
+
+
+
+                    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+                      <ol class="carousel-indicators">
+                        <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                        <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                        <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                      </ol>
                       <div class="carousel-inner">
-                      @foreach($about_images as $image)
-                        <div class="carousel-item active">
-                          <img src="{{ url(isset($image->name) ? $image->name : 'images/nopic.jpg') }}" alt="image" style="border-radius: 2px;max-width: 38em; max-height: auto; margin-top: 1.3em;"> 
-                          <div class="carousel-caption d-none d-md-block">
-                            <h5 class="text-dark">Globec Scientific Limited</h5>
-                          </div>
-                        </div>
-                      @endforeach
+
+                        @foreach($about_images as $image)
+                            <div class="carousel-item active">
+                              <img class="d-block w-100" src="{{ url(isset($image->name) ? $image->name : 'images/nopic.jpg') }}" alt="image" style="border-radius: 2px;max-width: 38em; max-height: auto; margin-top: 1.3em;"> 
+                              <div class="carousel-caption d-none d-md-block">
+                                <h5 class="text-dark">Globec Scientific Limited</h5>
+                              </div>
+                            </div>
+                          @endforeach
+
+
                       </div>
-                      <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+                      <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
                         <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                        <span class="visually-hidden">Previous</span>
-                      </button>
-                      <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+                        <span class="sr-only">Previous</span>
+                      </a>
+                      <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
                         <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                        <span class="visually-hidden">Next</span>
-                      </button>
+                        <span class="sr-only">Next</span>
+                      </a>
+                    </div>
                     </div>
 
             </div>
